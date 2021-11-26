7ad86b3d33d7ca6dfba79596a2f3cf4088568286
Matthias Schreiber <matthias.schreiber@zoho.com>
2021-11-26 19:29:26 -0300

    added wave shader for menu background

---
+++ assets/wave.glsl
@@ -0,0 +1,47 @@
+---VERTEX SHADER--- // vertex shader starts here
+#ifdef GL_ES
+    precision highp float;
+#endif
+
+/* Outputs to the fragment shader */
+varying vec4 frag_color;
+varying vec2 tex_coord0;
+
+/* vertex attributes */
+attribute vec2     vPosition;
+attribute vec2     vTexCoords0;
+
+/* uniform variables */
+uniform mat4       modelview_mat;
+uniform mat4       projection_mat;
+uniform vec4       color;
+
+void main (void) {
+  frag_color = color;
+  tex_coord0 = vTexCoords0;
+  gl_Position = projection_mat * modelview_mat * vec4(vPosition.xy, 0.0, 1.0);
+}
+
+
+---FRAGMENT SHADER--- // fragment shader starts here
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 resolution;
+uniform float time;
+
+float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+void main() {
+    //vec2 st = gl_FragCoord.xy/vec2(32, 32).xy;
+    vec2 st = floor(gl_FragCoord.xy/vec2(4, 4).xy);
+
+    vec3 color = vec3(0.);
+    //color = vec3(0.0, 0.48, 0.75) / vec3(abs(sin(time+st.x+sin(st.y)))*.5+.5);
+    color = vec3(0.0, 0.48, 0.75) / vec3((abs(sin(time*rand(st)))*.25+.75));
+
+    gl_FragColor = vec4(color,1.0);
+}
--- screens/mainmenu.py
+++ screens/mainmenu.py
@@ -1,8 +1,29 @@
+from kivy.core.window import Window
 from kivy.lang.builder import Builder
+from kivy.clock import Clock
+from kivy.graphics import RenderContext, Color, Rectangle
 from kivymd.uix.screen import MDScreen
+from kivy.uix.widget import Widget
 
 Builder.load_file('screens/mainmenu.kv')
 
 
 class MainMenu(MDScreen):
     pass
+    # def on_kv_post(self, base_widget):
+    #     self.background_.add_widget(Background())
+
+
+class Background(Widget):
+    def __init__(self, **kwargs):
+        self.canvas = RenderContext(use_parent_projection=True, use_parent_modelview=True)
+        self.canvas.shader.source = 'assets/wave.glsl'
+        super().__init__(**kwargs)
+        Clock.schedule_interval(self.update_shader, 0)
+
+    def update_shader(self, *args):
+        s = self.canvas
+        s['projection_mat'] = Window.render_context['projection_mat']
+        s['time'] = Clock.get_boottime()
+        s['resolution'] = list(map(float, self.size))
+        s.ask_update()
